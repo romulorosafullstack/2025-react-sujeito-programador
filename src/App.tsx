e6656d93137de460ae61cd4c20a277684fa598d0
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-22 12:12:13 -0300

    idade mais um attr em AlunoProps - passando na funcao Aluno - chamando App passando valor

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 // COMPONENT APP
-// Component em forma de functoion
+// Component em forma de function
+// OBS: Component com Props - Recomenda-se somente para componets que sejam aproveitados mais de uma vez
+
 
 // export default - por padrão pode ser enxergada/utilizada em outro componentes
 // function - função
@@ -13,25 +15,30 @@ export default function App() {
   return (
     <>
       <h1>Projeto Romulo</h1>
-      // Chamo a função Aluno
-      <Aluno nome = "Romulo" />
-      <Aluno nome = "Leonardo" />
-      <Aluno nome = "Flávio" />
+
+      // Chamo a função Aluno usando props e passando os valores refrentes a nome e idade como argumentos
+      <Aluno nome="Romulo" idade={40} />
+      <Aluno nome="Leonardo" idade={21} />
+      <Aluno nome="Flávio" idade={32} />
     </>
   )
 }
 
-// Pra poder usar props declaro uma interface pro component Aluno
+// Pra poder usar props na função, declaro uma interface pro component Aluno
 interface AlunoProps {
   nome: string;
+  idade: number
 }
 
 // No mesmo arquivo crio o componente/função Aluno
 // Para ver funcionando, chamo a função dentro do component App
-// Com a interface AlunoProps criada passo a prop nome na função Aluno
-function Aluno({nome}: AlunoProps) {
+// Com a interface AlunoProps criada passo suas props na função Aluno
+function Aluno({nome, idade}: AlunoProps) {
   return (
-    // Com a prop passada como argumento posso chamar pra que exiba pelo componente Aluno
-    <h2>Aluno: {nome}</h2>
+    <>
+      // Com as props passadas como argumentos posso chamar pra que exiba pelo componente Aluno
+      <h2>Aluno: {nome}</h2>
+      <h2>Idade: {idade}</h2>
+    </>
   )
 }
