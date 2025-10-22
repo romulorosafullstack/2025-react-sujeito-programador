d9da74186ae06a251dd7d8f6aa03f16f8bcc0677
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-22 13:30:17 -0300

    componencomponent header com import css

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,12 @@
-export function Header(){
+//import o css
+import './header.css'
+
+// Crio o component/função Header
+// export - para ser usado em outros componentes
+export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className='header__wrapper'>
+      <h1 className='header__title'>Header</h1>
     </header>
   );
 }
