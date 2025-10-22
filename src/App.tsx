6f4842f00aa61366c11fd00afdc8471829b34c69
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-22 15:43:25 -0300

    hook useState atualizando valor do input e exibindo

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,56 @@
+{/* HOOK useState */}
+{/* - Usar quando precisar de comportamento dinâmico */}
+{/* 1) importar o hook useState do react */ }
 import "./reset.css"
-import { Header } from "./components/Header";
-import { Aluno } from "./components/Aluno";
-import { Footer } from "./components/Footer";
+import { useState } from "react";
 
-
-// function - função
-// App - nome da função
-// () - inicializador da função
-// {} - Bloco de código
-// return - indicador de retorno da função
-// () - 
-// </> - fragmento do React para encapsular mais de um elemento
 export default function App() {
+
+  // DECLARANDO ESTADOS
+  // const - constante
+  // [input, setInput] = Array de estados [estadoInicial, estadoAtualizado]
+  // = Atribuição
+  // useState("") - cria estado inicial, no caso como vazio
+  const [input, setInput] = useState("")
+
+
+  // functin - função
+  // mostrarAluno - nome
+  // () - inicializador sewm param
+  // {} - Bloco de código que será executado
+  // alert(input) - Função que exibe um alerta passando o valor do estado input
+  function mostrarAluno() {
+    alert(input);
+  }
+
+
   return (
     <>
-      <Header
-        title="Romulo Alinhamentyo Smartts"
+      <h1>Estudando UseState</h1>
+
+      <input
+        type="text"
+        placeholder="Digite seu nome"
+        // Valor do input recebe o valor inicial do estado input
+        value={input}
+        // onChange - Evento disparado quando o input é alterado
+        // {} - Função executada quando o evento é disparado
+        // (e) - Parâmetro do evento
+        // => - Arrow function
+        // setInput(e.target.value) - Atualiza o estado input com estadoAtualizado = e.target.value
+        onChange={(e) => setInput(e.target.value)}
       />
-      <Aluno
-        nome="Romulo"
-        idade={40}
-      />
-      <Aluno
-        nome="Leonardo"
-        idade={21}
-      />
-      <Aluno
-        nome="Flávio"
-        idade={32}
-      />
-      <Footer
-        brand="Smartts Utilities"
-        copyright="Todos os direitos reservados"
-      />
+
+      <br />
+
+      {/* button - Botão */}
+      {/* onClick - método que executa algo mediante um clique */}
+      {/* {mostrarAluno} - função */}
+      <button onClick={mostrarAluno}>
+        Mostrar Aluno
+      </button>
+
+
     </>
   )
 }
