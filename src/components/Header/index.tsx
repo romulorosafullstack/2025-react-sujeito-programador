259a017eb40b6ff20999e41f848f8fcee756a831
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-22 14:45:20 -0300

    component header props

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 //import o css
 import './header.css'
 
+interface HeaderProps {
+  title: string;
+}
+
 // Crio o component/função Header
 // export - para ser usado em outros componentes
-export function Header() {
+export function Header({title}: HeaderProps) {
   return (
     <header className='header__wrapper'>
-      <h1 className='header__title'>Header</h1>
+      <h1 className='header__title'>{title}</h1>
     </header>
   );
 }
