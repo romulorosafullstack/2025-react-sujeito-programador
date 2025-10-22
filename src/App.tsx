d25ccdf8b04e0f38093e8d3e0d065124255a6af5
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-22 11:34:04 -0300

    component App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+// COMPONENT APP
+// Component em forma de functoion
+
+// export default - por padrão pode ser enxergada/utilizada em outro componentes
+// function - função
+// App - nome da função
+// () - inicializador da função
+// {} - Bloco de código
+// return - indicador de retorno da função
+// () - 
+// </> - fragmento do React para encapsular mais de um elemento
 export default function App() {
   return (
     <>
-      <h1>App</h1>
+      <h1>Projeto Romulo</h1>
     </>
   )
 }
