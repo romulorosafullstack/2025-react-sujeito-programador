6afa726ef10fff23fb538ef00b6a03673dff1001
Romulo Rosa | Front-End <romulorosafullstack@gmail.com>
2025-10-23 14:03:57 -0300

    hook useStates userLogin

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,44 @@
 {/* HOOK useState */}
-{/* - Usar quando precisar de comportamentos dinâmicos */ }
-
-{/* 1) importar o hook useState do react */ }
-//import "./reset.css"
+{/* - Usar quando precisar de comportamentos dinâmicos e mutável sem atualizar a página */}
+{/* - Todo state precisa ser declarado dentro do componente*/}
 import { useState } from "react";
+//import "./reset.css"
+
+interface UserProps{
+  nome: string;
+  cargo: string
+}
+
 
 export default function App() {
+  const [user, setUser] = useState<UserProps>({
+    nome: "Romulo",
+    cargo: "Front-End",
+  })
 
-  // Interface para os props do componente Aluno
-  interface AlunoInfoProps{
-    nome: string;
-    idade: string | number;
-  }
-
-  // DECLARANDO ESTADOS
-  // const - constante
-  // [aluno, setAluno] = Array de estados [estadoInicial, estadoAtualizado]
-  // = Atribuição
-  // useState("") - cria estado inicial, ("") no caso como vazio
-  const [nome, setNome] = useState("")
-  const [idade, setIdade] = useState("")
-
-  // Unificando duas states num objeto
-  const [alunoInfo, setAlunoInfo] = useState<AlunoInfoProps>()
-
-  // Declaro um state para o contado
-  const [contador, setContador] = useState(0)
-
-  // function - função
-  // mostrarAluno - nome
-  // () - inicializador sewm param
-  // {} - Bloco de código que será executado
-  // alert(input) - Função que exibe um alerta passando o valor do estado input
-  function mostrarAluno() {
-    setAlunoInfo({
-      nome: nome,
-      idade: idade
+  function handleLogin() {
+    setUser({
+      nome: "Romulo",
+      cargo: "Front-End",
     })
   }
 
-  function contadorAdicionar() {
-    setContador(valorAtual => valorAtual + 1)
-  }
-
-  function contadorSubtrair() {
-    if (contador === 0) {
-      return;
-    }
-    setContador(valorAtual => valorAtual - 1)
+  function handleLogout() {
+    setUser({
+      nome: "visitante",
+      cargo: "",
+    })
   }
 
 
   return (
     <>
       <h1>Estudando UseState</h1>
-
-      <input
-        type="text"
-        placeholder="Digite seu nome"
-        // Valor do input recebe o valor inicial do estado input
-        value={nome}
-        // onChange - Evento disparado quando o input é alterado
-        // {} - Função executada quando o evento é disparado
-        // (e) - Parâmetro do evento
-        // => - Arrow function
-        // setInput(e.target.value) - Atualiza o estado input com estadoAtualizado = e.target.value
-        onChange={(e) => setNome(e.target.value)}
-      />
-
-      <br />
-
-      <input
-        type="text"
-        placeholder="Digite sua idade"
-        value={idade}
-        onChange={(e) => setIdade(e.target.value)}
-      />
-
-      <br />
-
-      {/* button - Botão */}
-      {/* onClick - método que executa algo mediante um clique */}
-      {/* {mostrarAluno} - função */}
-      <button onClick={mostrarAluno}>
-        Mostrar Aluno
-      </button>
-
-      <h3>Bem-vindo: {alunoInfo?.nome}</h3>
-      <p>Idade: {alunoInfo?.idade} anos</p>
-
-      <hr />
-      <br />
-      <h1>Contador com useState</h1>
-      <button onClick={contadorAdicionar}> + </button>
-      {contador}
-      <button onClick={contadorSubtrair}> - </button>
+      <button onClick={handleLogin}>Entrar</button>
+      <button onClick={handleLogout}>Sair</button>
+      <h2>Olá {user.nome}!</h2>
+      <h3>{user.cargo}</h3>
+  
     </>
   )
 }
